545037ea69d67c404feb0da63322a325c13216c1
Pitt Wu <kgb00128@gmail.com>
2022-04-24 10:10:18 +0800

    fix: remove not use file

--- docs/Frontend/Next/Syntax/01-fetch-data.md
+++
@@ -1,5 +0,0 @@
----
-id: 01-fetch-data
-title: '☘️ Fetch Data'
-slug: /fetch-data
----
--- sidebar/Frontend/LeetCodeList.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: 'category',
-  label: 'LeetCode',
-  items: [
-    {
-      Low: ['Basics/LeetCode/low/low1-10'],
-    },
-  ],
-};
